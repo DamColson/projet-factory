15d68ecf6d6f5163efe956305e91738d2500f553
Steven LECLERC <s.leclerc076@outlook.fr>
2024-07-19 10:40:38 +0200

    Création Interceptor + config

---
+++ angular-factory/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  if (localStorage.getItem('token') != null) {
+    req = req.clone({
+      headers: req.headers.append(
+        'Authorization',
+        'Basic ' + localStorage.getItem('token')
+      ),
+    });
+  }
+
+  return next(req);
+};
