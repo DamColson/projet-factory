2f31c2120de2e47c4fb5537c01f6aed42113e9f1
DamColson <Damien.colson@gmail.com>
2024-07-19 10:30:24 +0200

    initialisation d'un compte user et d'un compte admin avec password encodé en BCrypt

---
+++ projet-factory/src/test/java/ajc/formation/projet_factory/AdminInit.java
@@ -0,0 +1,40 @@
+package ajc.formation.projet_factory;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import ajc.formation.projet_factory.dao.IDAOCompte;
+import ajc.formation.projet_factory.model.Compte;
+
+@SpringBootTest
+class AdminInit {
+	@Autowired
+	IDAOCompte daoCompte;
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Test
+	@Transactional
+	@Commit
+	@Disabled
+	void initAdmin() {
+		Compte admin=daoCompte.findById(1).get();
+		admin.setPassword( passwordEncoder.encode("admin"));
+		daoCompte.save(admin);
+	}
+	
+	@Test
+	@Transactional
+	@Commit
+	@Disabled
+	void initUser() {
+		Compte user=daoCompte.findById(2).get();
+		user.setPassword( passwordEncoder.encode("user"));
+		daoCompte.save(user);
+	}
+}
