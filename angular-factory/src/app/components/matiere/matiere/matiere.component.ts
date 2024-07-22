e47a0c640f6f312f36adb7391342da733c8f2de2
Stéphanie Canuel <canuel.stephanie@gmail.com>
2024-07-22 11:43:05 +0200

    ajout matiere

--- angular-factory/src/app/components/matiere/matiere/matiere.component.ts
+++ angular-factory/src/app/components/matiere/matiere/matiere.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Matiere } from '../../../model/matiere';
+import { MatiereService } from '../../../services/matiere.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-matiere',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './matiere.component.html',
-  styleUrl: './matiere.component.css'
+  styleUrl: './matiere.component.css',
 })
 export class MatiereComponent {
+  matieres: Matiere[] = [];
 
+  constructor(private matiereSrv: MatiereService) {
+    this.initMatiere();
+  }
+
+  delete(id: number) {
+    this.matiereSrv.delete(id).subscribe(() => {
+      this.initMatiere();
+    });
+  }
+
+  initMatiere() {
+    this.matiereSrv.getAll().subscribe((matieres) => {
+      this.matieres = matieres;
+    });
+  }
 }
