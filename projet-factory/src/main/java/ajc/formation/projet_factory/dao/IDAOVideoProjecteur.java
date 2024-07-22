a4aa50ef188132f9e74f1beb96f547653d0be649
Noah FIQUET <45941029+Slloth@users.noreply.github.com>
2024-07-22 13:10:46 +0200

    Update IDAOVideoProjecteur.java

--- projet-factory/src/main/java/ajc/formation/projet_factory/dao/IDAOVideoProjecteur.java
+++ projet-factory/src/main/java/ajc/formation/projet_factory/dao/IDAOVideoProjecteur.java
@@ -5,17 +5,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import ajc.formation.projet_factory.model.Formateur;
 import ajc.formation.projet_factory.model.Salle;
 import ajc.formation.projet_factory.model.VideoProjecteur;
 import jakarta.transaction.Transactional;
 
 public interface IDAOVideoProjecteur extends JpaRepository<VideoProjecteur, Integer> {
-	
-	@Query("update VideoProjecteur v set v.formateur=null where v.formateur = :formateur")
-	@Modifying
-	@Transactional
-	public void cascadeFormateurNull(@Param("formateur") Formateur formateur);
 	
 	@Query("update VideoProjecteur v set v.salle=null where v.salle = :salle")
 	@Modifying
