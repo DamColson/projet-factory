b6e02ba35435865b1afb104555bf22eb3552c4f6
Stéphanie Canuel <canuel.stephanie@gmail.com>
2024-07-22 12:35:10 +0200

    ajout video projecteur components

--- angular-factory/src/app/components/videoprojecteur/videoprojecteur/videoprojecteur.component.ts
+++ angular-factory/src/app/components/videoprojecteur/videoprojecteur/videoprojecteur.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Videoprojecteur } from '../../../model/videoprojecteur';
+import { VideoprojecteurService } from '../../../services/videoprojecteur.service';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-videoprojecteur',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, DatePipe],
   templateUrl: './videoprojecteur.component.html',
-  styleUrl: './videoprojecteur.component.css'
+  styleUrl: './videoprojecteur.component.css',
 })
 export class VideoprojecteurComponent {
+  videoprojecteurs: Videoprojecteur[] = [];
 
+  constructor(private videoprojecteurrSrv: VideoprojecteurService) {
+    this.initVideoprojecteur();
+  }
+
+  delete(id: number) {
+    this.videoprojecteurrSrv.delete(id).subscribe(() => {
+      this.initVideoprojecteur();
+    });
+  }
+
+  initVideoprojecteur() {
+    this.videoprojecteurrSrv.getAll().subscribe((videoprojecteurs) => {
+      this.videoprojecteurs = videoprojecteurs;
+    });
+  }
 }
---
+++ angular-factory/src/app/model/videoprojecteur.ts
@@ -0,0 +1,9 @@
+export class Videoprojecteur {
+  constructor(
+    public id?: number,
+    public adresse_mac?: number,
+    public date_achat?: Date,
+    public libelle?: string,
+    public status?: string
+  ) {}
+}
