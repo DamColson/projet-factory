7bd2f22e0b392d39ae63f0b5caf5773077527f7c
Slloth <fiquet.noah@gmail.com>
2024-07-22 12:40:09 +0200

    videoProjecteur

--- projet-factory/src/main/java/ajc/formation/projet_factory/dao/IDAOSalle.java
+++ projet-factory/src/main/java/ajc/formation/projet_factory/dao/IDAOSalle.java
@@ -1,9 +1,18 @@
 package ajc.formation.projet_factory.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import ajc.formation.projet_factory.model.Salle;
+import ajc.formation.projet_factory.model.VideoProjecteur;
 
 public interface IDAOSalle extends JpaRepository<Salle, Integer>{
 
+    @Query("update Salle s set s.video_projecteur=null where v.video_projecteur = :videoProjecteur")
+    @Modifying
+    @Transactional
+    public void cascadeVideoProjecteurNull(@Param("videoProjecteur") VideoProjecteur videoProjecteur);
 }
