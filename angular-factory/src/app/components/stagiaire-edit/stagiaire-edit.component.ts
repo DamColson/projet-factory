40697140283887418ad50d0972e8e497fa38fc06
Steven LECLERC <s.leclerc076@outlook.fr>
2024-07-22 15:38:16 +0200

    Edit Stagiaire Update

--- angular-factory/src/app/components/stagiaire-edit/stagiaire-edit.component.ts
+++ angular-factory/src/app/components/stagiaire-edit/stagiaire-edit.component.ts
@@ -1,12 +1,84 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { Ordinateur } from '../../model/ordinateur';
+import { Observable } from 'rxjs';
+import { Stagiaire } from '../../model/stagiaire';
+import { StagiaireService } from '../../services/stagiaire.service';
 
 @Component({
   selector: 'app-stagiaire-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink,
+    RouterLinkActive,
+    AsyncPipe,
+    ReactiveFormsModule,
+  ],
   templateUrl: './stagiaire-edit.component.html',
-  styleUrl: './stagiaire-edit.component.css'
+  styleUrl: './stagiaire-edit.component.css',
 })
 export class StagiaireEditComponent {
+  //filieres: Filiere[] = [];
+  form!: FormGroup;
+  OrdinateurObservable!: Observable<Ordinateur[]>;
 
+  stagiaire: Stagiaire = new Stagiaire();
+
+  constructor(
+    private stagiaireSrv: StagiaireService,
+    //private ordinateurSrv: OrdinateurService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.form = new FormGroup({
+      nom: new FormControl('', Validators.required),
+      prenom: new FormControl('', Validators.required),
+      mail: new FormControl('', Validators.required),
+      telephone: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    // this.filiereSrv.getAll().subscribe((filieres) => {
+    //   this.filieres = filieres;
+    // });
+    //this.OrdinateurObservable = this.ordinateurSrv.getAll();
+
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['id']) {
+        this.stagiaireSrv.getById(params['id']).subscribe((stagiaire) => {
+          this.stagiaire = stagiaire;
+        });
+      }
+    });
+  }
+  save() {
+    if (this.stagiaire.id) {
+      this.stagiaireSrv.update(this.stagiaire).subscribe((stagiaire) => {
+        this.router.navigateByUrl('/stagiaire?q=update&id=' + stagiaire.id);
+      });
+    } else {
+      this.stagiaireSrv.create(this.stagiaire).subscribe((stagiaire) => {
+        this.router.navigateByUrl('/stagiaire?q=create&id=' + stagiaire.id);
+      });
+    }
+  }
+
+  compareFn(o1: Ordinateur, o2: Ordinateur): boolean {
+    return o1 && o2 ? o1.id === o2.id : false;
+  }
 }
