12c1ec6c0ece3266bdafc42b855c8e363fe8d5a4
Steven LECLERC <s.leclerc076@outlook.fr>
2024-07-23 12:16:28 +0200

    Alert MAJ - MAJ Form Bloc

--- angular-factory/src/app/components/matiere/matiere/matiere.component.ts
+++ angular-factory/src/app/components/matiere/matiere/matiere.component.ts
@@ -1,31 +1,57 @@
 import { Component } from '@angular/core';
 import { Matiere } from '../../../model/matiere';
 import { MatiereService } from '../../../services/matiere.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-matiere',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './matiere.component.html',
   styleUrl: './matiere.component.css',
 })
 export class MatiereComponent {
   matieres: Matiere[] = [];
+  message = '';
+  showMessage = false;
+  style = 'info';
 
-  constructor(private matiereSrv: MatiereService) {
-    this.initMatiere();
+  constructor(
+    private matiereSrv: MatiereService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.initMatieres();
+    this.initMessage();
+  }
+
+  initMatieres() {
+    this.matiereSrv.getAll().subscribe((matieres) => {
+      this.matieres = matieres;
+    });
+  }
+
+  initMessage() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      if (params['q']) {
+        if (params['q'] == 'create') {
+          this.message = `Matière ${params['id']} crée `;
+          this.style = 'alert-success';
+        } else if (params['q'] == 'update') {
+          this.message = `Matière ${params['id']} mis à jour `;
+          this.style = 'alert-info';
+        }
+      }
+    });
+    this.showMessage = true;
   }
 
   delete(id: number) {
     this.matiereSrv.delete(id).subscribe(() => {
-      this.initMatiere();
-    });
-  }
-
-  initMatiere() {
-    this.matiereSrv.getAll().subscribe((matieres) => {
-      this.matieres = matieres;
+      this.initMatieres();
+      this.message = ` Matiere ${id} supprimé `;
+      this.style = 'alert-danger';
     });
   }
 }
--- angular-factory/src/app/components/salle/salle/salle.component.ts
+++ angular-factory/src/app/components/salle/salle/salle.component.ts
@@ -1,31 +1,57 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 import { Salle } from '../../../model/salle';
 import { SalleService } from '../../../services/salle.service';
 
 @Component({
   selector: 'app-salle',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './salle.component.html',
   styleUrl: './salle.component.css',
 })
 export class SalleComponent {
   salles: Salle[] = [];
+  message = '';
+  showMessage = false;
+  style = 'info';
 
-  constructor(private salleSrv: SalleService) {
-    this.initSalle();
+  constructor(
+    private salleSrv: SalleService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.initSalles();
+    this.initMessage();
+  }
+
+  initSalles() {
+    this.salleSrv.getAll().subscribe((salles) => {
+      this.salles = salles;
+    });
+  }
+
+  initMessage() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      if (params['q']) {
+        if (params['q'] == 'create') {
+          this.message = `Salle ${params['id']} crée `;
+          this.style = 'alert-success';
+        } else if (params['q'] == 'update') {
+          this.message = `Salle ${params['id']} mis à jour `;
+          this.style = 'alert-info';
+        }
+      }
+    });
+    this.showMessage = true;
   }
 
   delete(id: number) {
     this.salleSrv.delete(id).subscribe(() => {
-      this.initSalle();
-    });
-  }
-
-  initSalle() {
-    this.salleSrv.getAll().subscribe((salles) => {
-      this.salles = salles;
+      this.initSalles();
+      this.message = ` Salle ${id} supprimé `;
+      this.style = 'alert-danger';
     });
   }
 }
