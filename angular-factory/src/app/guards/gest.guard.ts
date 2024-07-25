3bb6db4d5019f4ad48909a4509a46c257da950f3
Steven LECLERC <s.leclerc076@outlook.fr>
2024-07-25 16:40:34 +0200

    Security du front

--- angular-factory/src/app/guards/gest.guard.ts
+++ angular-factory/src/app/guards/gest.guard.ts
@@ -1,9 +1,9 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const gestGuard: CanActivateFn = (route, state) => {
   let role = JSON.parse(localStorage.getItem('compte')!).role;
-  if (role != null) {
+  if (role == 'ROLE_GESTIONNAIRE' || role == 'ROLE_ADMIN') {
     return true;
   }
   return inject(Router).createUrlTree(['/login']);
--- angular-factory/src/app/guards/tech.guard.ts
+++ angular-factory/src/app/guards/tech.guard.ts
@@ -1,9 +1,9 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const techGuard: CanActivateFn = (route, state) => {
   let role = JSON.parse(localStorage.getItem('compte')!).role;
-  if (role != null) {
+  if (role == 'ROLE_TECHNICIEN' || role == 'ROLE_ADMIN') {
     return true;
   }
   return inject(Router).createUrlTree(['/login']);
